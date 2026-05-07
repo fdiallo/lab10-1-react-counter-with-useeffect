dfa8683ef31fddef5728770fcf21099edce6bd81
fdiallo <fallaye.diallo@gmail.com>
2026-05-06 22:54:27 -0400

    Implemented UI and completed logic for increment and decrement

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 import { ChevronUp, ChevronDown } from "lucide-react"
-import { width } from '@fortawesome/free-solid-svg-icons/fa0'
 
 function App() {
   const [count, setCount] = useState(0)
   const [step, setStep] = useState(1)
+
+  const decrement = () => setCount(prev => prev - step)
+  const increment = () => setCount(prev => prev + step)
+
+  const chevronUp = () => setStep(prev => prev + 1)
+  const chevronDown = () => setStep(prev => prev -1)
 
   return (
     <div>
@@ -13,8 +18,8 @@ function App() {
       <h2>Counter Count: {count}</h2>
 
       <div>
-        <button style={{ marginRight: "20px" }}>Decrement</button>
-        <button style={{ marginRight: "20px" }}>Increment</button>
+        <button style={{ marginRight: "20px" }} onClick={decrement}>Decrement</button>
+        <button style={{ marginRight: "20px" }} onClick={increment}>Increment</button>
         <button style={{ height: "40px", backgroundColor: "red" }}>Reset</button>
       </div><br />
 
@@ -24,10 +29,10 @@ function App() {
         <div style={{ display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center", alignContent: "center", border: "1px solid white", padding: "10px" }}>
           <p style={{ marginRight: "10px", fontSize: "24px", fontWeight: "bold"}}> {step}</p>
           <div style={{ display: "flex", flexDirection: "column" }}>
-            <button style={{cursor: 'pointer'}}>
+            <button style={{cursor: 'pointer'}} onClick={chevronUp}>
               <ChevronUp size={16} />
             </button>
-            <button style={{cursor: 'pointer'}}>
+            <button style={{cursor: 'pointer'}} onClick={chevronDown}>
               <ChevronDown size={16} />
             </button>
           </div>
